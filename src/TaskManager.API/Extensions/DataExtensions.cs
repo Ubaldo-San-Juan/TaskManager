45fb3cd6baf0b07c0da7500d14da92378a855dc7
Ubaldo-San-Juan <usan9fam@gmail.com>
2025-12-17 22:41:27 -0600

    feat: seeders created (Generate Migrations Fixes #14)

---
+++ src/TaskManager.API/Extensions/DataExtensions.cs
@@ -0,0 +1,29 @@
+﻿using TaskManager.Data.Context;
+using TaskManager.Data.Seeders;
+
+namespace TaskManager.API.Extensions
+{
+    public static class DataExtensions
+    {
+        public static async Task SeedDataAsync(this WebApplication app)
+        {
+            if (!app.Environment.IsDevelopment()) { return; }
+
+            using var scope = app.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var logger = services.GetRequiredService<ILogger<Program>>();
+
+            try
+            {
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                logger.LogInformation("Seeding the database...");
+                await DbSeeder.SeedAsync(context, 10, 5);
+                logger.LogInformation("Database seeding completed.");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while seeding the database.");
+            }
+        }
+    }
+}
