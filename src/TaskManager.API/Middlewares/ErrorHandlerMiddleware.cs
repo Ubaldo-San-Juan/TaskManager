3a2f07f17bc7b2cc9c4c80343724c46800bee344
Ubaldo-San-Juan <usan9fam@gmail.com>
2025-12-18 01:04:00 -0600

    feat: generate Middleware and Api response (#16)

---
+++ src/TaskManager.API/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,64 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using TaskManager.Business.Common;
+
+namespace TaskManager.API.Middlewares
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlerMiddleware> _logger;
+
+        public ErrorHandlerMiddleware(RequestDelegate next, ILogger<ErrorHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                var response = context.Response;
+
+                ApiResponse<string> responseModel;
+
+                switch (ex)
+                {
+                    case ValidationException e:
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        
+                        var errors = e.Errors.Select(x => x.ErrorMessage).ToList();
+                        responseModel = new ApiResponse<string>(e.Message);
+                    break;
+
+                    case KeyNotFoundException e:
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        responseModel = new ApiResponse<string>(e.Message);
+                        break;
+
+                    case InvalidOperationException e:
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        responseModel = new ApiResponse<string>(e.Message);
+                    break;
+
+                    default:
+                        _logger.LogError(ex, ex.Message);
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        responseModel = new ApiResponse<string>("An error happend into the server");
+                        break;
+                }
+
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                await response.WriteAsync(JsonSerializer.Serialize(responseModel, options));
+            }
+        }
+    }
+}
