49742a3e14eb90cac3af473d61fdf9e476c5301f
Ubaldo-San-Juan <usan9fam@gmail.com>
2026-01-18 02:11:38 -0600

    feat: add AuthController with login endpoint

---
+++ src/TaskManager.API/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Business.Common;
+using TaskManager.Business.DTOs.Auth;
+using TaskManager.Business.Interfaces;
+
+namespace TaskManager.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<ApiResponse<AuthResponseDto>>> Login([FromBody] LoginDto loginDto)
+        {
+            var result = await _authService.LoginAsync(loginDto);
+
+            var response = new ApiResponse<AuthResponseDto>(result, "Login success");
+            return Ok(response);
+        }
+    }
+}
