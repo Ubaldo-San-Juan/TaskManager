1b5124d721d6e262535c17f43f84b07976dd45c8
Ubaldo-San-Juan <usan9fam@gmail.com>
2026-01-25 02:35:29 -0600

    feat: create Role repository #32

---
+++ src/TaskManager.Data/Interfaces/IRoleRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Data.Entities;
+
+namespace TaskManager.Data.Interfaces
+{
+    public interface IRoleRepository
+    {
+        Task<IEnumerable<Role>> GetAllRoleAsync();
+        Task<Role> GetRoleByIdAsync(int id);
+        Task<Role> GetRoleByNameAsync(string name);
+        Task CreateRoleAsync(Role role);
+        Task UpdateRoleAsync(Role role);
+        Task DeleteRoleAsync(int id);
+
+    }
+}
