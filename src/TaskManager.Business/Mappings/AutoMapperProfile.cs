bce9c28fb57efdff849b0e08ebffa4fd9c7757db
Ubaldo-San-Juan <usan9fam@gmail.com>
2026-02-15 00:05:20 -0600

    feat: Implement AutoMapper profiles for DTO-entity mapping and FluentValidation for task DTOs.

--- src/TaskManager.Business/Mappings/AutoMapperProfile.cs
+++ src/TaskManager.Business/Mappings/AutoMapperProfile.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaskManager.Business.DTOs.Roles;
+using TaskManager.Business.DTOs.Tasks;
 using TaskManager.Business.DTOs.Users;
 using TaskManager.Data.Entities;
 
@@ -16,19 +17,24 @@ namespace TaskManager.Business.Mappings
         {
             // Mapping for User Entity and DTOs
             CreateMap<User, UserDto>();
-
             CreateMap<CreateUserDto, User>()
                 .ForMember(dest => dest.PasswordHash, opt => opt.Ignore());
-
             CreateMap<UpdateUserDto, User>()
                 .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
                 .ForMember(dest => dest.CreatedAt, opt => opt.Ignore());
-
 
             // Mapping for Role Entity and DTOs
             CreateMap<Role, RoleDto>();
             CreateMap<CreateRoleDto, Role>();
             CreateMap<UpdateRoleDto, Role>();
+
+            // Mapping for TodoTask Entity and DTOs
+            CreateMap<TodoTask, TodoTaskDto>();
+            CreateMap<CreateTaskDto, TodoTask>();
+            CreateMap<UpdateTaskDto, TodoTask>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore());
         }
     }
 }
