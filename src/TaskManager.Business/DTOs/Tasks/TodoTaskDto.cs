2f0d04cecf285cd43d12901e81c0e645b7a3bfbf
Ubaldo-San-Juan <usan9fam@gmail.com>
2026-02-22 23:58:23 -0600

    feat: Add priority and due date fields to tasks with corresponding DTOs, validation, and database migration.

--- src/TaskManager.Business/DTOs/Tasks/TodoTaskDto.cs
+++ src/TaskManager.Business/DTOs/Tasks/TodoTaskDto.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManager.Data.Enums;
 
 namespace TaskManager.Business.DTOs.Tasks
 {
@@ -12,6 +14,9 @@ namespace TaskManager.Business.DTOs.Tasks
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public bool IsCompleted { get; set; } = false;
+
+        public DateTime? DueDate { get; set; }
+        public TaskPriority Priority { get; set; }
         public int UserId { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
--- src/TaskManager.Business/DTOs/Tasks/UpdateTaskDto.cs
+++ src/TaskManager.Business/DTOs/Tasks/UpdateTaskDto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManager.Data.Enums;
 
 namespace TaskManager.Business.DTOs.Tasks
 {
@@ -11,5 +12,8 @@ namespace TaskManager.Business.DTOs.Tasks
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public bool IsCompleted { get; set; } = false;
+
+        public DateTime? DueDate { get; set; }
+        public TaskPriority Priority { get; set; } = TaskPriority.Medium;
     }
 }
